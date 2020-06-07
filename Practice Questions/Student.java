8811a229f4dd0241eecb1a09cb359b379af040e7
Shanthosh-KP <60930729+Shanthosh-KP@users.noreply.github.com>
2020-06-07 20:10:23 +0530

    Create Student.java

---
+++ Practice Questions/Student.java
@@ -0,0 +1,25 @@
+public class Person //Parent Class
+{
+	String name;
+	int age;
+	String qualification;
+	public Person()
+	{
+		name = "Steve Rogers";
+		age = 45;
+		qualification = "B.E";
+	}
+}
+
+class Student//Child Class
+{
+	String name;
+	int age;
+	String School;
+	public Student()
+	{
+		name = "Bucky Barnes";
+		age  = 20;
+		School = "Queens School";
+	}
+}
