58d0e14d75e1dd070c77caf8577e302bec93d592
Shanthosh-KP <60930729+Shanthosh-KP@users.noreply.github.com>
2020-06-09 19:01:05 +0530

    Create Srev.java

---
+++ Practice Questions/Srev.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Srev {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		StringBuilder rev=new StringBuilder();
+		int x=s.nextInt();
+		int temp;
+		while(x>0) {
+			temp=x%10;
+			rev.append(temp);
+			x/=10;
+		}
+		System.out.println(rev);
+		s.close();
+	}
+}
